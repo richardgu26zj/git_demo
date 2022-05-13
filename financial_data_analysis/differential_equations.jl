cfb77ea7502f5c5263b9d56fb30a03aee5215239
Richard X. Gu <richardgu26@outlook.com>
2022-05-13 22:25:32 +0800

    differential equations with Julia notebook

---
+++ financial_data_analysis/differential_equations.jl
@@ -0,0 +1,16 @@
+using DifferentialEquations
+
+function rober!(du,u,p,t)
+    y1,y2,y3 = u
+    k1,k2,k3 = p
+    du[1] = -k1*y1 + k3*y2*y3
+    du[2] = k1*y1 - k3*y2*y3 - k2*y2^2
+    du[3] = k2*y2^2
+    nothing
+end
+
+u = [1.0,0.0,0.0]
+tspan = (0.0,1e5)
+params = [0.04,3e7,1e4]
+
+ode_prob = ODEProblem(rober!,u,tspan,params)
